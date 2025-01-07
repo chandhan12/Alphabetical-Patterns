68769e6096ec6ae8ba542219de8cf4c8c8ddf995
chandhan <chandhan.g0401@gmail.com>
2025-01-07 18:16:33 +0530

    updated

--- pattern-B.py
+++ pattern-B.py
@@ -2,7 +2,7 @@ r=5
 c=5
 for i in range(r):
     for j in range(c):
-        if(i==0 or j==0 or j==4 or i==2 or i==4):
+        if(((i==0 or i==4 or i==2)and j<4) or j==0 or (j==4 and (i==1 or i==3)) ):
             print("*",end=" ")
         else:
             print(" ",end=" ")
--- pattern-V.py
+++ pattern-V.py
@@ -2,7 +2,7 @@ r=5
 c=5
 for i in range(r):
     for j in range(c):
-        if((i==1 and (j==0 or j==4)) or (i==2 and (j==1 or j==3)) or (i==3 and j==2)):
+        if((i==j and j<3) or  (j==c-1-i and i<3)):
             print("*",end=" ")
         else:
             print(" ",end=" ")
